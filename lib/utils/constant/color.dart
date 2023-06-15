52ad29460c100c25dec093fa7e07632535c31874
usamadev2 <usamadev2@gmail.com>
2023-06-15 20:01:44 +0500

    .

--- lib/utils/constant/color.dart
+++ lib/utils/constant/color.dart
@@ -7,8 +7,6 @@ class AppColors {
   static const Color textColor2 = Color(0xff667085);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
-  static const Color red = Color(0xffEA2B2B);
-  static const Color orange = Color(0xffFF9600);
   static const Color lightGreen = Color(0xff15DCDB);
   static const Color shadowlightGreen = Color.fromARGB(255, 9, 194, 194);
   static const Color dividerColor = Color(0xffE7E8EC);
